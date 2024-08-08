e50526ba167a28ea9e3c5a5f8b216de8f5a975b7
Diogo Dourado <diogo@dourado.net>
2024-08-08 19:05:34 -0300

    Personalizando a StatusBar

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native';
+import { Text, View, StatusBar } from 'react-native';
 import { useFonts, Roboto_700Bold, Roboto_400Regular } from '@expo-google-fonts/roboto';
 
 export default function App() {
@@ -6,7 +6,8 @@ export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular });
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "#202024" }}>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
       {fontsLoaded ? <Text>Home</Text> : <View />}
     </View>
   );
