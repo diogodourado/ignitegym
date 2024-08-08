4a64b73c630d52391840d6d3294fe646091a4e7a
Diogo Dourado <diogo@dourado.net>
2024-08-08 19:23:26 -0300

    Instalando o Gluestack / Customizando o Tema padrão

--- App.tsx
+++ App.tsx
@@ -1,14 +1,22 @@
-import { Text, View, StatusBar } from 'react-native';
+import { View, StatusBar } from 'react-native';
 import { useFonts, Roboto_700Bold, Roboto_400Regular } from '@expo-google-fonts/roboto';
+import { GluestackUIProvider, Text, Center } from '@gluestack-ui/themed';
+import { config } from './config/gluestack-ui.config';
 
 export default function App() {
 
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular });
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "#202024" }}>
+    <GluestackUIProvider config={config}>
+
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-      {fontsLoaded ? <Text>Home</Text> : <View />}
-    </View>
+      {fontsLoaded ? (
+        <Center flex={1} bg='$info600'>
+          <Text>Home</Text>
+        </Center>
+      ) : <View />}
+
+    </GluestackUIProvider>
   );
 }
