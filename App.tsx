a1b494b32bd5c5aa34901f0746ef21eccdca2530
Diogo Dourado <diogo@dourado.net>
2024-08-12 13:29:34 -0300

    Imagem de Background

--- App.tsx
+++ App.tsx
@@ -1,8 +1,11 @@
 import { StatusBar } from 'react-native';
 import { useFonts, Roboto_700Bold, Roboto_400Regular } from '@expo-google-fonts/roboto';
 import { GluestackUIProvider, Text, Center } from '@gluestack-ui/themed';
+
 import { config } from './config/gluestack-ui.config';
+
 import { Loading } from '@components/Loading';
+import { SignIn } from '@screens/Signin';
 
 export default function App() {
 
@@ -12,13 +15,7 @@ export default function App() {
     <GluestackUIProvider config={config}>
 
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-      {fontsLoaded ? (
-        <Center flex={1} bg='$info600'>
-          <Text>Home</Text>
-        </Center>
-      ) : (
-        <Loading />
-      )}
+      {fontsLoaded ? <SignIn /> : (<Loading />)}
 
     </GluestackUIProvider>
   );
